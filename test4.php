80a6100c909c3ed476d78755f62ade7a2ec30d86
Ricky <a686868520@gmail.com>
2016-08-23 10:01:08 +0000

    驗證炸彈

--- test4.php
+++ test4.php
@@ -1,57 +1,100 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
 $str=$_GET['map'];
-$str1=str_replace('N',"",$str);
-// echo $str1;
-// exit;
-$strMnum=substr_count($str,'M');
 
+$str1=str_replace('N',"",$str);
+
+$strMnum=substr_count($str,'M');
+$strmnum=substr_count($str,'m');
 $strNnum=substr_count($str,'N');
 
 $torf=1;
 
 $mapStr=str_split($str);
 $mapStr1=str_split($str1);
+// var_dump($mapStr1);
 $strNum=count($mapStr);
-
-
 
 
 if($strNum<109 || $strNum>109)
 {
-    echo "不符合,因為字串超過或少於109個<br>";
+    echo "字串為".$strNum."。";
     $torf=0;
 }
-else if(is_null($str))
-{
-    echo "不符合,因為是NULL<br>";
-    $torf=0;
-}
-else if(empty($str))
-{
-    echo "不符合,因為是空值<br>";
-    $torf=0;
-}
-else if(!isset($str))
-{
-    echo "不符合,因為變數不存在<br>";
-    $torf=0;
-}
-else if($strNnum>9 || $strNnum<9)
-{
 
-    echo "不符合,因為N超過或少於9個<br>";
-    $torf=0;
-}
-else if($strMnum>40 || $strMnum<40)
-{
-    $torf=0;
-    echo "不符合,因為M超過或少於40個<br>";
-}
 else
 {
+    echo "字串長度正確,共109個。";
+}
+
+if(!preg_match("/^([0-8MN]+)$/",$str))
+{
+    echo "輸入格式不是0-8或MN。";
+    $torf=0;
+}
+
+if(strpos($str,'m'))
+{
+    echo "地雷大小寫有錯。";
+}
+
+if(is_null($str))
+{
+    echo "是NULL。";
+    $torf=0;
+}
+
+if(!is_string($str))
+{
+    echo "是不字串。";
+    $torf=0;
+}
+
+if(empty($str))
+{
+    echo "是空值。";
+    $torf=0;
+}
+
+if(!isset($str))
+{
+    echo "變數不存在。";
+    $torf=0;
+}
+
+if($strNnum>9 || $strNnum<9)
+{
+
+    echo "N數量有錯，有".$strNnum."個N。";
+    $torf=0;
+}
+
+if($strMnum>40 || $strMnum<40)
+{
+    if($strMnum+$strmnum!=40)
+    {
+        $torf=0;
+        echo "地雷數量有錯，有".($strMnum+$strmnum)."顆地雷。";
+    }
+}
+
+if($mapStr1)
+{
+    for($i=0;$i<10;$i++)
+    {
+
+        if($i==9 && $mapStr[$i*11+10]==='N')
+        {
+            echo "最後不能有N。";
+            break;
+        }
+
+
+
+
+    }
     $q=0;
-    // var_dump($mapStr);
+
     for($i=0;$i<10;$i++)
     {
         for($j=0;$j<10;$j++) {
@@ -68,56 +111,56 @@ else
             for($j=0;$j<10;$j++) {
                 $num=0;
 
-                if($QQ[$i][$j]==='M')
+                if($QQ[$i][$j]==='M' || $QQ[$i][$j]==='m')
                 {
                     continue;
                 }
 
-                if($QQ[$i-1][$j]==='M')
+                if($QQ[$i-1][$j]==='M' || $QQ[$i-1][$j]==='m')
                 {
                     $num++;
                 }
 
-                if($QQ[$i+1][$j]==='M')
+                if($QQ[$i+1][$j]==='M' || $QQ[$i+1][$j]==='m')
                 {
                     $num++;
                 }
 
-                if($QQ[$i][$j-1]==='M')
+                if($QQ[$i][$j-1]==='M' || $QQ[$i][$j-1]==='m')
                 {
                     $num++;
                 }
 
-                if($QQ[$i-1][$j-1]==='M')
+                if($QQ[$i-1][$j-1]==='M' || $QQ[$i-1][$j-1]==='m')
                 {
                     $num++;
                 }
 
-                if($QQ[$i+1][$j-1]==='M')
+                if($QQ[$i+1][$j-1]==='M' || $QQ[$i+1][$j-1]==='m')
                 {
                     $num++;
                 }
 
-                if($QQ[$i][$j+1]==='M')
+                if($QQ[$i][$j+1]==='M' || $QQ[$i][$j+1]==='m')
                 {
                     $num++;
                 }
 
-                if($QQ[$i+1][$j+1]==='M')
+                if($QQ[$i+1][$j+1]==='M' || $QQ[$i+1][$j+1]==='m')
                 {
                     $num++;
                 }
 
-                if($QQ[$i-1][$j+1]==='M')
+                if($QQ[$i-1][$j+1]==='M' || $QQ[$i-1][$j+1]==='m')
                 {
                     $num++;
                 }
 
                 if($num!=$QQ[$i][$j])
                 {
-                    echo "不符合,因為炸彈數量不對位置在座標".$i.$j;
+                    echo "座標[".$i."][".$j."]炸彈數量不對,應該為".$num."。";
                     $torf=0;
-                    break 2;
+
                 }
 
 
@@ -134,9 +177,11 @@ if($torf==1)
 
 }
 
+if($torf==0)
+{
+    echo "結果不符合";
 
-
-
+}
 
 
 
